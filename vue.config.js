ea58fa043d4a2b14951eca40cdb4e1630d2c734e
yixiangxiao <13479793571@163.com>
2020-11-25 13:37:24 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = {
     publicPath: './',
     outputDir: "dist", // 输出文件目录
     lintOnSave: false, // eslint 是否在保存时检查
-    assetsDir: 'static', // 配置js、css静态资源二级目录的位置  
     devServer: {
         port: 8080,
         host: 'localhost',
